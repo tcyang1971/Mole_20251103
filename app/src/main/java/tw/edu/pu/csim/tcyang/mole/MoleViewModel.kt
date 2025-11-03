acbe7f299a5279f0544e2b4d06174d4c0041a52e
tcyang1971 <tcyang@gm.pu.edu.tw>
2025-11-03 16:31:31 +0800

    v5_地鼠隨機移動

--- app/src/main/java/tw/edu/pu/csim/tcyang/mole/MoleViewModel.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/mole/MoleViewModel.kt
@@ -2,7 +2,9 @@ package tw.edu.pu.csim.tcyang.mole
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.IntSize
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
@@ -15,6 +17,19 @@ class MoleViewModel: ViewModel() {
 
     var stay by mutableLongStateOf(0)
         private set // 設定為 private
+
+    var maxX by mutableStateOf(0)
+        private set
+
+    var maxY by mutableStateOf(0)
+        private set
+
+    var offsetX by mutableStateOf(0)
+        private set
+
+    var offsetY by mutableStateOf(0)
+        private set
+
 
 
     fun incrementCounter() {
@@ -31,8 +46,22 @@ class MoleViewModel: ViewModel() {
             while (true) { // 無限循環，每秒增加一次
                 delay(1000L)
                 stay++ // 計數器加 1，這會自動觸發 UI 更新
+                moveMole()
             }
         }
+    }
+
+    // 根據螢幕寬度,高度及地鼠圖片大小,計算螢幕範圍
+    fun getArea(gameSize: IntSize, moleSize:Int) {
+        maxX = gameSize.width - moleSize
+        maxY = gameSize.height - moleSize
+    }
+
+
+    // 根據螢幕寬度,高度及地鼠圖片大小,隨機移動地鼠不超出螢幕範圍
+    fun moveMole() {
+        offsetX = (0..maxX).random()
+        offsetY = (0..maxY).random()
     }
 
 
